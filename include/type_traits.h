e64da6361629adfec9493d58bfa4639a927dfdd2
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-01-12 21:48:03 +0300

    Initial commit; Added type_traits.h

---
+++ include/type_traits.h
@@ -0,0 +1,53 @@
+#ifndef _YATO_TYPE_TRAITS_H_
+#define _YATO_TYPE_TRAITS_H_
+
+#include <type_traits>
+#include <memory>
+
+//----------------------------------------------------------
+// Smart pointer traits
+//
+namespace yato
+{
+    namespace details
+    {
+        template<typename T, template<typename...> class SmartPtr, typename Enable = void>
+        struct is_smart_ptr_impl 
+        {
+            static constexpr bool value = false;
+        };
+
+        template<typename T, template<typename...> class SmartPtr>
+        struct is_smart_ptr_impl<T, SmartPtr, typename std::enable_if< std::is_same<typename std::remove_cv<T>::type, SmartPtr< typename T::element_type> >::value >::type > 
+        {
+            static constexpr bool value = true;
+        };
+    }
+    
+    /**
+     * Detect shared_ptr
+     * is_shared_ptr<T>::value is true when T is shared_ptr<V> for some V
+     */
+    template<typename T>
+    using is_shared_ptr = details::is_smart_ptr_impl<T, std::shared_ptr>;
+    
+    /**
+     * Detect unique_ptr
+     * is_unique_ptr<T>::value is true when T is is_unique_ptr<V> for some V
+     */
+    template<typename T>
+    using is_unique_ptr = details::is_smart_ptr_impl<T, std::unique_ptr>;
+    
+    /**
+     * Detect shared_ptr or unique_ptr
+     * auto_ptr is evil - don't use it
+     * weak_ptr has a little bit different behavior... so I dont want to mix it with others
+     */
+    template<typename T>
+    struct is_smart_ptr 
+    {
+        static constexpr bool value = is_shared_ptr<T>::value || is_unique_ptr<T>::value;
+    };
+}
+
+#endif
