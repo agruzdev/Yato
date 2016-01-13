efe9fb8626c1acb69a52655c5c1318f3da9e3901
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-01-13 18:25:26 +0300

    Added not_null wrapper

---
+++ include/not_null.h
@@ -0,0 +1,116 @@
+/**
+ * YATO library
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Alexey Gruzdev
+ */
+
+#ifndef _YATO_NOT_NULL_
+#define _YATO_NOT_NULL_
+
+#include "type_traits.h"
+
+namespace yato
+{ 
+    /**
+     * not_null wrapper forcing function argument to be not nullptr
+     */
+    template<typename T, typename Enable = void>
+    class not_null
+    {};
+    
+    /**
+     * Implementation for raw pointers
+     */
+    template<typename T>
+    class not_null<T, typename std::enable_if< std::is_pointer<T>::value >::type >
+    {    
+        T m_pointer;
+    public:
+        constexpr not_null(T ptr):
+            m_pointer(ptr) 
+        {
+            if(ptr == nullptr){
+                throw std::runtime_error("not_null: null pointer exception!");
+            }
+        }
+        
+        not_null(nullptr_t ptr) = delete;
+        
+        constexpr operator T () noexcept {
+            return m_pointer;
+        }
+        
+        constexpr auto get() noexcept {
+            return m_pointer;
+        }
+        
+        constexpr const auto get() const noexcept {
+            return m_pointer;
+        }
+        
+        constexpr auto operator->() noexcept {
+            return m_pointer;
+        }
+        
+        constexpr const auto operator->() const noexcept {
+            return m_pointer;
+        }
+        
+        constexpr auto& operator*() /*noexcept?*/ {
+            return *m_pointer;
+        }
+        
+        constexpr const auto& operator*() const /*noexcept?*/ {
+            return *m_pointer;
+        }
+    };
+    /**
+     * Implementation for smart pointers
+     */
+    template<typename T>
+    class not_null<T, typename std::enable_if< is_smart_ptr<T>::value >::type >
+    {    
+        const T & m_smart_pointer;
+    public:
+        constexpr not_null(const T& ptr):
+            m_smart_pointer(ptr) 
+        {
+            if(ptr.get() == nullptr){
+                throw std::runtime_error("not_null: null pointer exception!");
+            }
+        }
+        
+        not_null(nullptr_t ptr) = delete;
+        
+        constexpr operator T () noexcept {
+            return m_smart_pointer;
+        }
+        
+        constexpr auto get() noexcept {
+            return m_smart_pointer.get();
+        }
+        
+        constexpr const auto get() const noexcept {
+            return m_smart_pointer.get();
+        }
+        
+        constexpr auto operator->() noexcept {
+            return m_smart_pointer.operator->();
+        }
+        
+        constexpr const auto operator->() const noexcept {
+            return m_smart_pointer.operator->();
+        }
+        
+        constexpr auto& operator*() /*noexcept?*/ {
+            return *m_smart_pointer;
+        }
+        
+        constexpr const auto& operator*() const /*noexcept?*/ {
+            return *m_smart_pointer;
+        }
+    };
+
+}
+#endif
