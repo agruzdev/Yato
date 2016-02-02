1e870b73bcc17736aec72e503df35163c8abdabc
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-02-02 22:36:58 +0300

    vector_nd: added construction from initializer_list

--- include/yato/vector_nd.h
+++ include/yato/vector_nd.h
@@ -18,6 +18,20 @@ namespace yato
    
     namespace details
     {
+        /*
+         * Make type of multidimensional initializer list
+         */
+        template<typename _T, size_t _Dims>
+        struct initilizer_list_nd
+        {
+            using type = std::initializer_list<typename initilizer_list_nd<_T, _Dims - 1>::type>;
+        };
+
+        template<typename _T>
+        struct initilizer_list_nd<_T, 1>
+        {
+            using type = std::initializer_list<_T>;
+        };
 
 
         template <typename _DataType, size_t _DimensionsNum, typename _Allocator>
@@ -27,6 +41,9 @@ namespace yato
             using my_type = vector_nd_impl<_DataType, _DimensionsNum, _Allocator>;
             using data_type = _DataType;
             using allocator_type = _Allocator;
+            using container_type = std::vector<data_type, allocator_type>;
+            using iterator = typename container_type::iterator;
+            using const_iterator = typename container_type::const_iterator;
 
             static YATO_CONSTEXPR_VAR size_t dimensions_num = _DimensionsNum;
             static_assert(dimensions_num > 1, "Implementation for dimensions number larger than 1");
@@ -35,15 +52,58 @@ namespace yato
             using sizes_array = std::array<size_t, dimensions_num>;
             using size_iterator = typename sizes_array::iterator;
 
+            template<size_t _Dims>
+            using initilizer_type = typename initilizer_list_nd<data_type, _Dims>::type;
+
             sizes_array m_dimensions;
             sizes_array m_sub_sizes;
-            std::vector<data_type, allocator_type> m_plain_vector;
+            container_type m_plain_vector;
 
-            void _init_sizes(const std::initializer_list<size_t> & sizes) YATO_NOEXCEPT_KEYWORD{
-                std::copy(sizes.begin(), sizes.end(), m_dimensions.begin());
+            void _init_subsizes() YATO_NOEXCEPT_KEYWORD
+            {
                 m_sub_sizes[dimensions_num - 1] = m_dimensions[dimensions_num - 1];
                 for (size_t i = dimensions_num - 1; i > 0; --i) {
                     m_sub_sizes[i - 1] = m_dimensions[i - 1] * m_sub_sizes[i];
+                }
+            }
+
+            void _init_sizes(const std::initializer_list<size_t> & sizes) YATO_NOEXCEPT_KEYWORD
+            {
+                std::copy(sizes.begin(), sizes.end(), m_dimensions.begin());
+                _init_subsizes();
+            }
+
+            template<size_t _Depth>
+            auto _init_sizes(const initilizer_type<_Depth> & init_list) YATO_NOEXCEPT_KEYWORD
+                -> typename std::enable_if<(_Depth > 1), void>::type
+            {
+                m_dimensions[dimensions_num - _Depth] = init_list.size();
+                _init_sizes<_Depth-1>(*(init_list.begin()));
+            }
+
+            template<size_t _Depth>
+            auto _init_sizes(const initilizer_type<_Depth> & init_list) YATO_NOEXCEPT_KEYWORD
+                -> typename std::enable_if<(_Depth == 1), void>::type
+            {
+                m_dimensions[dimensions_num - _Depth] = init_list.size();
+               _init_subsizes();
+            }
+
+            template<size_t _Depth>
+            auto _init_values(const initilizer_type<_Depth> & init_list, iterator & iter) YATO_NOEXCEPT_KEYWORD
+                -> typename std::enable_if<(_Depth > 1), void>::type
+            {
+                for (const auto & init_sub_list : init_list) {
+                    _init_values<_Depth - 1>(init_sub_list, iter);
+                }
+            }
+
+            template<size_t _Depth>
+            auto _init_values(const initilizer_type<_Depth> & init_list, iterator & iter) YATO_NOEXCEPT_KEYWORD
+                -> typename std::enable_if<(_Depth == 1), void>::type
+            {
+                for (const auto & value : init_list) {
+                    *iter++ = value;
                 }
             }
 
@@ -88,6 +148,20 @@ namespace yato
                 }
                 _init_sizes(sizes);
                 m_plain_vector.resize(m_sub_sizes[0], value);
+            }
+
+            /**
+             *	Create from initializer list
+             */
+            vector_nd_impl(const initilizer_type<dimensions_num> & init_list)
+                : m_plain_vector()
+            {
+                _init_sizes<dimensions_num>(init_list);
+                if (m_sub_sizes[0] > 0) {
+                    m_plain_vector.resize(m_sub_sizes[0]);
+                    auto iter = m_plain_vector.begin();
+                    _init_values<dimensions_num>(init_list, iter);
+                }
             }
 
             /**
--- tests/test_vector_nd.cpp
+++ tests/test_vector_nd.cpp
@@ -12,6 +12,10 @@ TEST(Yato_VectorND, ctor)
         yato::vector_nd<int, 2> vec1({ 2, 3 });
         yato::vector_nd<int, 1> vec2({ 5 }, 1);
         yato::vector_nd<float, 3> vec3({ 2, 3, 3 }, 11.0f);
+
+        yato::vector_nd<int, 2> vec4 = { {1, 1, 1}, {2, 2, 2} };
+        yato::vector_nd<int, 3> vec5 = { { {1, 1}, {1, 2}, {1, 3} }, { {2, 4}, {2, 5}, {2, 6} } };
+        yato::vector_nd<int, 2> vec6 = { };
     }
     catch (...)
     {
