a562e0005334e0798bec2c01ac396ee8fba46f2a
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-02-21 11:25:19 +0300

    Added singleton abstraction

---
+++ include/yato/singleton.h
@@ -0,0 +1,77 @@
+/**
+* YATO library
+*
+* The MIT License (MIT)
+* Copyright (c) 2016 Alexey Gruzdev
+*/
+
+#ifndef _YATO_SINGLETON_H_
+#define _YATO_SINGLETON_H_
+
+#include <memory>
+
+namespace yato
+{
+    
+    /**
+     *  Create helper
+     */
+    template <typename _T>
+    struct create_using_new
+    {
+        static _T* create()
+        {
+            return new _T;
+        }
+
+        static void destroy(_T* ptr)
+        {
+            delete ptr;
+        }
+    };
+
+    /**
+     *  Singleton abstraction
+     *  Simplified approach from Loki library
+     */
+    template<typename _T, template <typename> class _Creator>
+    class singleton_holder
+    {
+        static std::unique_ptr<_T> ms_instance;
+
+    public:
+        /**
+         *  Get singleton instance
+         */
+        static _T* instance()
+        {
+            touch();
+            return ms_instance.get();
+        }
+
+        /**
+         *  Get singleton instance
+         */
+        static _T& instance_ref()
+        {
+            touch();
+            return *ms_instance.get();
+        }
+
+        /**
+         *  Make sure that the singleton instance is created
+         */
+        static void touch()
+        {
+            if (ms_instance.get() == nullptr) {
+                ms_instance.reset(_Creator<_T>::create());
+            }
+        }
+    };
+
+    template<typename _T, template <typename> class _Creator>
+    std::unique_ptr<_T> singleton_holder<_T, _Creator>::ms_instance;
+
+}
+
+#endif
