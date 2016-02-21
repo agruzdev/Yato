e95738abfab6006a24bea3973dbb8919e0d0e236
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-02-21 14:27:14 +0300

    meta: added list of types

--- include/yato/meta.h
+++ include/yato/meta.h
@@ -15,22 +15,121 @@ namespace yato
 {
     namespace meta
     {
+        //-------------------------------------------------------
+        // meta::number
+        //
 
         /**
          *  Wrapper around unsigned integer value
          */
         template<uint32_t _Num>
-        struct Number: public Number<_Num - 1>
+        struct number: public number<_Num - 1>
         {
             static YATO_CONSTEXPR_VAR uint32_t value = _Num;
         };
 
         template<>
-        struct Number<0>
+        struct number<0>
         {
             static YATO_CONSTEXPR_VAR uint32_t value = 0;
         };
 
+
+
+        //-------------------------------------------------------
+        // meta::list
+        //
+
+        /**
+         *  Empty list of types
+         */
+        struct null_list {};
+
+        /**
+         *  List for aggregating a sequence of types
+         */
+        template<typename _Head, typename... _Tail>
+        struct list
+        {
+            using head = _Head;
+            using tail = list<_Tail...>;
+        };
+
+        /**
+         *  List consisting of one element
+         */
+        template<typename _Head>
+        struct list<_Head>
+        {
+            using head = _Head;
+            using tail = null_list;
+        };
+
+        /**
+         *  Reverse list
+         */
+        template<typename _List, typename... _Elems>
+        struct reverse_list
+        {
+            using type = typename reverse_list<typename _List::tail, typename _List::head, _Elems...>::type;
+        };
+
+        template<typename... _Elems>
+        struct reverse_list<null_list, _Elems...>
+        {
+            using type = list<_Elems...>;
+        };
+
+        /**
+         *  Add list element to the begin
+         */
+        template<typename _T, typename _List, typename... _Elems>
+        struct list_push_front
+        {
+            using type = typename list_push_front<_T, typename _List::tail, _Elems..., typename _List::head>::type;
+        };
+
+        template<typename _T, typename... _Elems>
+        struct list_push_front<_T, null_list, _Elems...>
+        {
+            using type = list<_T, _Elems...>;
+        };
+
+        /**
+         *  Add list element to the end
+         */
+        template< typename _List, typename _T, typename... _Elems>
+        struct list_push_back
+        {
+            using type = typename list_push_back<typename _List::tail, _T, _Elems..., typename _List::head>::type;
+        };
+
+        template<typename _T, typename... _Elems>
+        struct list_push_back<null_list, _T, _Elems...>
+        {
+            using type = list<_Elems..., _T>;
+        };
+
+        /**
+         *  Concatenate two lists
+         */
+        template<typename _List1, typename _List2, typename... _Elems>
+        struct list_cat
+        {
+            using type = typename list_cat<typename _List1::tail, _List2, _Elems..., typename _List1::head>::type;
+        };
+
+        template<typename _List2, typename... _Elems>
+        struct list_cat<null_list, _List2, _Elems...>
+        {
+            using type = typename list_cat<null_list, typename _List2::tail, _Elems..., typename _List2::head>::type;
+        };
+
+        template<typename... _Elems>
+        struct list_cat<null_list, null_list, _Elems...>
+        {
+            using type = list<_Elems...>;
+        };
     }
 }
 
--- tests/test_meta.cpp
+++ tests/test_meta.cpp
@@ -1,11 +1,45 @@
 #include "gtest/gtest.h"
 
-#include <memory>
-#include <initializer_list>
 #include <yato/meta.h>
 
-
-TEST(Yato_Meta, counter)
+namespace
 {
+    template<typename _List>
+    struct print_list
+    {
+        static void print()
+        {
+            std::cout << typeid(typename _List::head).name() << ", ";
+            print_list<typename _List::tail>::print();
+        }
+    };
 
+    template<>
+    struct print_list<yato::meta::null_list>
+    {
+        static void print()
+        {
+            std::cout << std::endl;
+        }
+    };
+}
+
+TEST(Yato_Meta, list)
+{
+    using l1 = yato::meta::list<int, void, float>;
+    using l2 = typename yato::meta::list_push_front<double, l1>::type;
+    using l3 = typename yato::meta::reverse_list<l2>::type;
+    using l4 = typename yato::meta::list_push_back<l3, long>::type;
+
+    static_assert(std::is_same<l2, yato::meta::list<double, int, void, float>>::value, "yato::meta::list fail!");
+    static_assert(std::is_same<l3, yato::meta::list<float, void, int, double>>::value, "yato::meta::list fail!");
+    static_assert(std::is_same<l4, yato::meta::list<float, void, int, double, long>>::value, "yato::meta::list fail!");
+
+    using l5 = typename yato::meta::list_cat< yato::meta::list<int, short>, yato::meta::list<float, double> >::type;
+    using l6 = typename yato::meta::list_cat< yato::meta::list<int, short>, yato::meta::null_list >::type;
+    using l7 = typename yato::meta::list_cat< yato::meta::null_list, yato::meta::list<float, double> >::type;
+
+    static_assert(std::is_same<l5, yato::meta::list<int, short, float, double>>::value, "yato::meta::list fail!");
+    static_assert(std::is_same<l6, yato::meta::list<int, short>>::value, "yato::meta::list fail!");
+    static_assert(std::is_same<l7, yato::meta::list<float, double>>::value, "yato::meta::list fail!");
 }
