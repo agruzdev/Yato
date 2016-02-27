3895069f6f76f2be3bfda551900d071ee6a9dd61
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-02-27 20:14:30 +0300

    Fixed interface of tuple algorithms

--- include/yato/tuple.h
+++ include/yato/tuple.h
@@ -21,39 +21,25 @@ namespace yato
         {
             template <template <typename> class _Func>
             YATO_CONSTEXPR_FUNC
-            static auto apply(const _Tuple & tuple)
+            static auto apply(_Tuple tuple)
                 -> decltype(tuple_transform_impl<_Tuple, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(tuple))
             {
                 return tuple_transform_impl<_Tuple, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(tuple);
-            }
-
-            template <template <typename> class _Func>
-            YATO_CONSTEXPR_FUNC
-            static auto apply(_Tuple && tuple)
-                -> decltype(tuple_transform_impl<_Tuple, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(std::move(tuple)))
-            {
-                return tuple_transform_impl<_Tuple, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(std::move(tuple));
             }
         };
 
         template<typename _Tuple, size_t... _Idxs>
         struct tuple_transform_impl<_Tuple, 0, _Idxs...>
         {
-            template <template <typename> class _Func>
-            YATO_CONSTEXPR_FUNC
-            static auto apply(const _Tuple & tuple)
-                -> decltype(std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple>::type>{}(std::get<_Idxs>(tuple))...))
-            {
-                return std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple>::type>{}(std::get<_Idxs>(tuple))...);
-            }
+            using tuple_pure_type = typename std::decay<_Tuple>::type;
 
-            //Will compile only if tuple has 1 element
             template <template <typename> class _Func>
             YATO_CONSTEXPR_FUNC
-            static auto apply(_Tuple && tuple)
-                -> decltype(std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple>::type>{}(std::get<_Idxs>(std::move(tuple)))...))
+            static auto apply(_Tuple tuple)
+                -> std::tuple<decltype(_Func<typename std::tuple_element<_Idxs, tuple_pure_type>::type>{}(std::get<_Idxs>(tuple)))...>
             {
-                return std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple>::type>{}(std::get<_Idxs>(std::move(tuple)))...);
+                return std::tuple<decltype(_Func<typename std::tuple_element<_Idxs, tuple_pure_type>::type>{}(std::get<_Idxs>(tuple)))...>(
+                    _Func<typename std::tuple_element<_Idxs, tuple_pure_type>::type>{}(std::get<_Idxs>(tuple))...);
             }
         };
 
@@ -64,9 +50,9 @@ namespace yato
     template<template <typename> class _Func, typename _Tuple>
     YATO_CONSTEXPR_FUNC
     auto tuple_transform(_Tuple && tuple)
-        -> decltype(details::tuple_transform_impl<typename std::decay<_Tuple>::type, std::tuple_size<typename std::decay<_Tuple>::type>::value>::template apply<_Func>(std::forward<_Tuple>(tuple)))
+        -> decltype(details::tuple_transform_impl<_Tuple, std::tuple_size<typename std::decay<_Tuple>::type>::value>::template apply<_Func>(std::forward<_Tuple>(tuple)))
     {
-        return details::tuple_transform_impl<typename std::decay<_Tuple>::type, std::tuple_size<typename std::decay<_Tuple>::type>::value>::template apply<_Func>(std::forward<_Tuple>(tuple));
+        return details::tuple_transform_impl<_Tuple, std::tuple_size<typename std::decay<_Tuple>::type>::value>::template apply<_Func>(std::forward<_Tuple>(tuple));
     }
 
 
@@ -77,7 +63,7 @@ namespace yato
         {
             template <template <typename, typename> class _Func>
             YATO_CONSTEXPR_FUNC
-            static auto apply(const _Tuple1 & tuple1, const _Tuple2 & tuple2)
+            static auto apply(_Tuple1 tuple1, _Tuple2 tuple2)
                 -> decltype(tuple_transform_2_impl<_Tuple1, _Tuple2, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(tuple1, tuple2))
             {
                 return tuple_transform_2_impl<_Tuple1, _Tuple2, _Length - 1, _Length - 1, _Idxs...>::template apply<_Func>(tuple1, tuple2);
@@ -87,12 +73,16 @@ namespace yato
         template<typename _Tuple1, typename _Tuple2, size_t... _Idxs>
         struct tuple_transform_2_impl<_Tuple1, _Tuple2, 0, _Idxs...>
         {
+            using tuple1_pure_type = typename std::decay<_Tuple1>::type;
+            using tuple2_pure_type = typename std::decay<_Tuple2>::type;
+
             template <template <typename, typename> class _Func>
             YATO_CONSTEXPR_FUNC
-            static auto apply(const _Tuple1 & tuple1, const _Tuple2 & tuple2)
-                -> decltype(std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple1>::type, typename std::tuple_element<_Idxs, _Tuple2>::type>{}(std::get<_Idxs>(tuple1), std::get<_Idxs>(tuple2))...))
+            static auto apply(_Tuple1 tuple1, _Tuple2 tuple2)
+                -> std::tuple<decltype(_Func<typename std::tuple_element<_Idxs, tuple1_pure_type>::type, typename std::tuple_element<_Idxs, tuple2_pure_type>::type>{}(std::get<_Idxs>(tuple1), std::get<_Idxs>(tuple2)))...>
             {
-                return std::make_tuple(_Func<typename std::tuple_element<_Idxs, _Tuple1>::type, typename std::tuple_element<_Idxs, _Tuple2>::type>{}(std::get<_Idxs>(tuple1), std::get<_Idxs>(tuple2))...);
+                return std::tuple<decltype(_Func<typename std::tuple_element<_Idxs, tuple1_pure_type>::type, typename std::tuple_element<_Idxs, tuple2_pure_type>::type>{}(std::get<_Idxs>(tuple1), std::get<_Idxs>(tuple2)))...>
+                    (_Func<typename std::tuple_element<_Idxs, tuple1_pure_type>::type, typename std::tuple_element<_Idxs, tuple2_pure_type>::type>{}(std::get<_Idxs>(tuple1), std::get<_Idxs>(tuple2))...);
             }
         };
 
@@ -104,9 +94,9 @@ namespace yato
     YATO_CONSTEXPR_FUNC
     auto tuple_transform(_Tuple1 && tuple1, _Tuple2 && tuple2)
         -> typename std::enable_if<(std::tuple_size<typename std::decay<_Tuple1>::type>::value == std::tuple_size<typename std::decay<_Tuple2>::type>::value),
-            decltype(details::tuple_transform_2_impl<typename std::decay<_Tuple1>::type, typename std::decay<_Tuple2>::type, std::tuple_size<typename std::decay<_Tuple1>::type>::value>::template apply<_Func>(std::forward<_Tuple1>(tuple1), std::forward<_Tuple2>(tuple2)))>::type
+            decltype(details::tuple_transform_2_impl<_Tuple1, _Tuple2, std::tuple_size<typename std::decay<_Tuple1>::type>::value>::template apply<_Func>(std::forward<_Tuple1>(tuple1), std::forward<_Tuple2>(tuple2)))>::type
     {
-        return details::tuple_transform_2_impl<typename std::decay<_Tuple1>::type, typename std::decay<_Tuple2>::type, std::tuple_size<typename std::decay<_Tuple1>::type>::value>::template apply<_Func>(std::forward<_Tuple1>(tuple1), std::forward<_Tuple2>(tuple2));
+        return details::tuple_transform_2_impl<_Tuple1, _Tuple2, std::tuple_size<typename std::decay<_Tuple1>::type>::value>::template apply<_Func>(std::forward<_Tuple1>(tuple1), std::forward<_Tuple2>(tuple2));
     }
 
     namespace details
