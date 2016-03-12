9ad955087c7f5a881f31d42e24789b45b4c1a722
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-03-12 19:19:36 +0300

    Added transform_iterator

---
+++ include/yato/transform_iterator.h
@@ -0,0 +1,373 @@
+/**
+* YATO library
+*
+* The MIT License (MIT)
+* Copyright (c) 2016 Alexey Gruzdev
+*/
+
+#ifndef _YATO_TRANSFORM_ITERATOR_H_
+#define _YATO_TRANSFORM_ITERATOR_H_
+
+#include "prerequisites.h"
+#include "type_traits.h"
+
+namespace yato
+{
+    template <typename _UnaryFunction, typename _Iterator>
+    class transform_iterator
+    {
+    public:
+        using unary_function_type = _UnaryFunction;
+        using iterator_type = _Iterator;
+        using my_type = transform_iterator<unary_function_type, iterator_type>;
+
+        //-------------------------------------------------------
+        // Definitions for iterator_traits
+        /**
+        * Category is same as iterator's category
+        */
+        using iterator_category = typename std::iterator_traits<iterator_type>::iterator_category;
+        /**
+         * Dereferencing the iterator yields the same type as calling the function
+         */
+        //using reference = typename yato::function_trait<unary_function_type>::result_type;
+        using reference = typename std::conditional<!std::is_same<iterator_category, std::output_iterator_tag>::value,
+            decltype((std::declval<unary_function_type>())(*(std::declval<iterator_type>()))), decltype(*(std::declval<iterator_type>()))>::type;
+        /**
+         * Value type preserves cv-qualifiers 
+         */
+        using value_type = typename std::remove_reference<reference>::type;
+        /**
+         * Pointer to value
+         */
+        using pointer = value_type*;
+        /**
+         * Difference type is same as iterator's difference type
+         * Using std::ptrdiff_t for output_iterator_tag to avoid compilation error for operator +=
+         */
+        using difference_type = typename std::conditional<!std::is_same<iterator_category, std::output_iterator_tag>::value, 
+            typename std::iterator_traits<iterator_type>::difference_type, std::ptrdiff_t>::type;
+        //-------------------------------------------------------
+
+    private:
+        iterator_type m_iterator;
+        unary_function_type m_function;
+        //-------------------------------------------------------
+
+    public:
+        /**
+         *  Create from iterator and function
+         */
+        YATO_CONSTEXPR_FUNC
+        transform_iterator(const iterator_type & iter, const unary_function_type & function)
+            : m_iterator(iter), m_function(function)
+        { }
+        
+        /**
+         *  Create from iterator and function
+         */
+        transform_iterator(iterator_type && iter, const unary_function_type & function)
+            : m_iterator(std::move(iter)), m_function(function)
+        { }
+
+        /**
+         *  Create from iterator and function
+         */
+        transform_iterator(const iterator_type & iter, unary_function_type && function)
+            : m_iterator(iter), m_function(std::move(function))
+        { }
+
+        /**
+         *  Create from iterator and function
+         */
+        transform_iterator(iterator_type && iter, unary_function_type && function)
+            : m_iterator(std::move(iter)), m_function(std::move(function))
+        { }
+
+        /**
+         *  Copy 
+         */
+        template<typename _AnotherUnaryFunction, typename _AnotherIterator>
+        YATO_CONSTEXPR_FUNC
+        transform_iterator(const transform_iterator<_AnotherUnaryFunction, _AnotherIterator> & other,
+            typename std::enable_if<(std::is_convertible<_AnotherUnaryFunction, unary_function_type>::value && std::is_convertible<_AnotherIterator, iterator_type>::value)>::type* = nullptr)
+            : m_iterator(other.m_iterator), m_function(other.m_function)
+        { }
+        
+        YATO_CONSTEXPR_FUNC
+        transform_iterator(const my_type & other)
+            : m_iterator(other.m_iterator), m_function(other.m_function)
+        { }
+
+        /**
+         *  Move
+         */
+        transform_iterator(my_type && other)
+            : m_iterator(std::move(other.m_iterator)), m_function(std::move(other.m_function))
+        { }
+
+        /**
+         *  Destroy
+         */
+        ~transform_iterator()
+        { }
+
+        /**
+         *  Copy
+         */
+        template<typename _AnotherUnaryFunction, typename _AnotherIterator>
+        auto operator = (const transform_iterator<_AnotherUnaryFunction, _AnotherIterator> & other)
+            -> typename std::enable_if<(std::is_convertible<_AnotherUnaryFunction, unary_function_type>::value && std::is_convertible<_AnotherIterator, iterator_type>::value), my_type &>::type
+        {
+            my_type tmp(other);
+            tmp.swap(*this);
+            return *this;
+        }
+
+        my_type & operator = (const my_type & other)
+        {
+            if (this != &other) {
+                my_type tmp(other);
+                tmp.swap(*this);
+            }
+            return *this;
+        }
+
+        /**
+         *  Swap iterators
+         */
+        void swap(my_type & other) YATO_NOEXCEPT_KEYWORD
+        {
+            using std::swap;
+            if (this != &other) {
+                swap(m_iterator, other.m_iterator);
+                swap(m_function, other.m_function);
+            }
+        }
+
+        /**
+         *  Move
+         */
+        my_type & operator = (my_type && other)
+        {
+            if (this != &other) {
+                m_iterator = std::move(other.m_iterator);
+                m_function = std::move(other.m_function);
+            }
+        }
+
+        /**
+         *  Dereference 
+         */
+        YATO_CONSTEXPR_FUNC
+        const reference operator*() const
+        {
+            return m_function(*m_iterator);
+        }
+
+        /**
+         *  Dereference
+         */
+        reference operator*()
+        {
+            return m_function(*m_iterator);
+        }
+
+        /**
+         *  Increment
+         */
+        my_type & operator++ ()
+        {
+            ++m_iterator;
+            return *this;
+        }
+
+        /**
+         *  Increment
+         */
+        my_type operator++ (int)
+        {
+            auto copy(*this);
+            ++(*this);
+            return copy;
+        }
+
+        /**
+         *  Decrement 
+         */
+        template<typename _MyCategory = iterator_category>
+        auto operator-- ()
+            -> typename std::enable_if<std::is_base_of<std::bidirectional_iterator_tag, _MyCategory>::value, my_type &>::type
+        {
+            --m_iterator;
+            return *this;
+        }
+
+        /**
+         *  Decrement 
+         */
+        template<typename _MyCategory = iterator_category>
+        auto operator-- (int)
+            -> typename std::enable_if<std::is_base_of<std::bidirectional_iterator_tag, _MyCategory>::value, my_type>::type
+        {
+            auto copy(*this);
+            --(*this);
+            return copy;
+        }
+
+        /**
+         *  Shift by offset
+         */
+        template<typename _MyCategory = iterator_category>
+        auto operator += (difference_type offset)
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, my_type &>::type
+        {
+            m_iterator += offset;
+            return *this;
+        }
+
+        /**
+         *  Shift by offset
+         */
+        template<typename _MyCategory = iterator_category>
+        auto operator -= (difference_type offset)
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, my_type &>::type
+        {
+            return this->operator+=(-offset);
+        }
+
+        /**
+         *  Shift by offset
+         */
+        template<typename _MyCategory = iterator_category>
+        YATO_CONSTEXPR_FUNC
+        auto operator + (difference_type offset) const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, my_type>::type
+        {
+            auto copy(*this);
+            return (copy += offset);
+        }
+
+        /**
+         *  Get distance 
+         */
+        template<typename _MyCategory = iterator_category>
+        auto operator - (const my_type & other)  const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, difference_type>::type
+        {
+            return m_iterator - other.m_iterator;
+        }
+
+        /**
+         *  Compare equality
+         */
+        YATO_CONSTEXPR_FUNC
+        bool operator == (const my_type & other)  const
+        {
+            return m_iterator == other.m_iterator;
+        }
+
+        /**
+         *  Compare inequality
+         */
+        YATO_CONSTEXPR_FUNC
+        bool operator != (const my_type & other) const
+        {
+            return !(*this == other);
+        }
+
+        /**
+         *  Compare less
+         */
+        template<typename _MyCategory = iterator_category>
+        YATO_CONSTEXPR_FUNC
+        auto operator < (const my_type & other) const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, bool>::type
+        {
+            return m_iterator < other.m_iterator;
+        }
+
+        /**
+         *  Compare greater
+         */
+        template<typename _MyCategory = iterator_category>
+        YATO_CONSTEXPR_FUNC
+        auto operator > (const my_type & other) const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, bool>::type
+        {
+            return m_iterator > other.m_iterator;
+        }
+
+        /**
+         *  Compare less or equal
+         */
+        template<typename _MyCategory = iterator_category>
+        YATO_CONSTEXPR_FUNC
+        auto operator <= (const my_type & other) const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, bool>::type
+        {
+            return !(*this > other);
+        }
+
+        /**
+         *  Compare greater or equal
+         */
+        template<typename _MyCategory = iterator_category>
+        YATO_CONSTEXPR_FUNC
+        auto operator >= (const my_type & other) const
+            -> typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, _MyCategory>::value, bool>::type
+        {
+            return !(*this < other);
+        }
+
+        // Make any other iterator friend to access fields in copy operator
+        template <typename _SomeFunction, typename _SomeIterator>
+        friend class transform_iterator;
+     };
+
+    template<typename _UnaryFunction, typename _Iterator>
+    void swap(transform_iterator<_UnaryFunction, _Iterator> & one, transform_iterator<_UnaryFunction, _Iterator> & another)
+    {
+        one.swap(another);
+    }
+
+#ifdef YATO_MSVC_2015
+    template<typename _UnaryFunction, typename _Iterator>
+    YATO_CONSTEXPR_FUNC
+    auto make_transform_iterator(_Iterator && iterator, _UnaryFunction && function)
+        -> transform_iterator<typename callable_trait<typename std::remove_reference<_UnaryFunction>::type>::function_type, typename std::remove_reference<_Iterator>::type>
+    {
+        return transform_iterator<typename callable_trait<typename std::remove_reference<_UnaryFunction>::type>::function_type, typename std::remove_reference<_Iterator>::type>(std::forward<_Iterator>(iterator), std::forward<_UnaryFunction>(function));
+    }
+#else
+    /**
+     *  ToDo: Add lambda support for MSVC 2013. Currently it is difficult due to poor SFINAE support
+     */
+    template<typename _UnaryFunction, typename _Iterator>
+    YATO_CONSTEXPR_FUNC
+    auto make_transform_iterator(_Iterator && iterator, _UnaryFunction && function)
+        -> transform_iterator<typename function_trait<typename std::remove_reference<_UnaryFunction>::type>::function_type, typename std::remove_reference<_Iterator>::type>
+    {
+        return transform_iterator<typename function_trait<typename std::remove_reference<_UnaryFunction>::type>::function_type, typename std::remove_reference<_Iterator>::type>(std::forward<_Iterator>(iterator), std::forward<_UnaryFunction>(function));
+    }
+#endif
+
+    template<typename _Result, typename _Arg, typename _Iterator>
+    YATO_CONSTEXPR_FUNC
+    auto make_transform_iterator(_Iterator && iterator, _Result(*function)(_Arg))
+        -> transform_iterator<std::function<_Result(_Arg)>, typename std::remove_reference<_Iterator>::type>
+    {
+        return transform_iterator<std::function<_Result(_Arg)>, typename std::remove_reference<_Iterator>::type>(std::forward<_Iterator>(iterator), function);
+    }
+
+    template<typename _UnaryFunction, typename _Iterator>
+    YATO_CONSTEXPR_FUNC
+    auto make_transform_iterator(_Iterator && iterator)
+        -> typename std::enable_if< std::is_constructible<_UnaryFunction>::value, transform_iterator<_UnaryFunction, typename std::remove_reference<_Iterator>::type> >::type
+    {
+        return transform_iterator<_UnaryFunction, typename std::remove_reference<_Iterator>::type>(std::forward<_Iterator>(iterator), _UnaryFunction());
+    }
+
+}
+
+#endif
---
+++ tests/test_transform_iterator.cpp
@@ -0,0 +1,144 @@
+#include "gtest/gtest.h"
+
+#include <iostream>
+#include <list>
+#include <forward_list>
+#include <numeric>
+#include <yato/transform_iterator.h>
+#include <yato/zip_iterator.h>
+
+TEST(Yato_TransformIterator, base)
+{
+    using input_iterator = std::istream_iterator<int>;
+    using output_iterator = std::ostream_iterator<int>;
+    using forward_iterator = std::forward_list<int>::iterator;
+    using bidirectional_iterator = std::list<int>::iterator;
+    using random_access_iterator = int*;
+
+    static_assert(std::is_same<yato::transform_iterator<std::function<int(int)>, input_iterator>::iterator_category, std::input_iterator_tag>::value, "zip_iterator trait failed");
+    static_assert(std::is_same<yato::transform_iterator<std::function<int(output_iterator&)>, output_iterator>::iterator_category, std::output_iterator_tag>::value, "zip_iterator trait failed");
+    static_assert(std::is_same<yato::transform_iterator<std::function<int(int)>, forward_iterator>::iterator_category, std::forward_iterator_tag>::value, "zip_iterator trait failed");
+    static_assert(std::is_same<yato::transform_iterator<std::function<int(int)>, bidirectional_iterator>::iterator_category, std::bidirectional_iterator_tag>::value, "zip_iterator trait failed");
+    static_assert(std::is_same<yato::transform_iterator<std::function<int(int)>, random_access_iterator>::iterator_category, std::random_access_iterator_tag>::value, "zip_iterator trait failed");
+}
+
+TEST(Yato_TransformIterator, increment)
+{
+    std::vector<int> a = { 0, 1, 2, 3, 4 };
+    
+    auto increment = [](const int & x) {return x + 1; };
+    yato::transform_iterator<std::function<int(const int&)>, std::vector<int>::iterator> it(a.begin(), increment);
+    
+    for (int i = 0; i < 5; ++i, ++it) {
+        EXPECT_EQ(i + 1, *it);
+    }
+}
+
+TEST(Yato_TransformIterator, decrement)
+{
+    std::vector<int> a = { 0, 1, 2, 3, 4 };
+    auto increment = [](const int & x) {return x + 1; };
+
+    yato::transform_iterator<std::function<int(const int&)>, std::vector<int>::iterator> it(std::prev(a.end()), increment);
+#ifndef YATO_MSVC_2013
+    for (int i = 5; i > 1; --i, --it) {
+#else
+    // In MSVC 2013 templated operator-- leads to crash due to a bug 
+    for (int i = 5; i > 1; --i, it.operator--()) {
+#endif
+        EXPECT_EQ(i, *it);
+    }
+}
+
+TEST(Yato_TransformIterator, loop)
+{
+    std::vector<int> a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    
+    auto erase = [](int & x) { int tmp = x; x = 0; return tmp; };
+
+    using iter_type = yato::transform_iterator<std::function<int(int&)>, std::vector<int>::iterator>;
+    auto iter = iter_type(a.begin(), erase);
+    auto end = iter_type(a.end(), erase);
+    for (int i = 1; iter != end; ++iter, ++i) {
+        EXPECT_EQ(i, *iter);
+    }
+    for (int x : a) {
+        EXPECT_EQ(0, x);
+    }
+}
+
+namespace
+{
+    struct SomeOp
+    {
+        float operator()(int && x) const {
+            return x + 1.0f;
+        }
+    };
+
+    static int increment_foo(int & x)
+    {
+        return x + 1;
+    }
+}
+
+TEST(Yato_TransformIterator, make_transform_iterator)
+{
+    std::vector<int> a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    const std::vector<int> b = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    auto increment = [](const int & x) {return x + 1; };
+    auto fun = std::function<int(const int&)>(increment);
+
+#ifdef YATO_MSVC_2015
+    auto iter1 = yato::make_transform_iterator(b.begin(), increment);
+#else
+    auto iter1 = yato::make_transform_iterator(b.begin(), std::function<int(const int&)>(increment));
+#endif
+    auto iter2 = yato::make_transform_iterator(b.begin(), fun);
+    auto iter3 = yato::make_transform_iterator(a.begin(), &increment_foo);
+    auto iter4 = yato::make_transform_iterator<SomeOp>(std::make_move_iterator(a.begin()));
+    
+#ifdef YATO_MSVC_2015
+    auto iter5 = yato::make_transform_iterator(b.begin(), increment);
+#else
+    auto iter5 = yato::make_transform_iterator(b.begin(), std::function<int(const int&)>(increment));
+#endif
+    iter1 = iter5;
+    
+    auto void_foo = [](int) -> void {};
+
+#ifdef YATO_MSVC_2015
+    auto iter6 = yato::make_transform_iterator(a.cbegin(), void_foo);
+    auto iter7 = yato::make_transform_iterator(a.begin(), void_foo);
+#else
+    auto iter6 = yato::make_transform_iterator(a.cbegin(), std::function<void(int)>(void_foo));
+    auto iter7 = yato::make_transform_iterator(a.begin(), std::function<void(int)>(void_foo));
+#endif
+    iter6 = iter7;
+}
+
+TEST(Yato_TransformIterator, zip_transform)
+{
+    const std::vector<int> a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    const std::vector<long> b = { 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+    
+#ifdef YATO_MSVC_2015
+    auto multiply = [](const std::tuple<int, long> & x) {
+        return std::get<0>(x) * std::get<1>(x);
+    };
+#else
+    std::function<long(const std::tuple<int, long> &)> multiply = [](const std::tuple<int, long> & x) {
+        return std::get<0>(x) * std::get<1>(x);
+    };
+#endif
+    
+    auto trIt  = yato::make_transform_iterator(yato::make_zip_iterator(a.begin(), b.begin()), multiply);
+    auto trEnd = yato::make_transform_iterator(yato::make_zip_iterator(a.end(), b.end()), multiply);
+    
+    float sum = std::accumulate(trIt, trEnd, 0.0f);
+    
+    EXPECT_EQ(735, sum);
+}
+
+
