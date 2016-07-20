98f5fac2cf25982c03484c03c9afadca2998eb80
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-07-20 19:37:41 +0300

    Added interval_map (basic implementation)

---
+++ include/yato/interval_map.h
@@ -0,0 +1,152 @@
+/**
+* YATO library
+*
+* The MIT License (MIT)
+* Copyright (c) 2016 Alexey Gruzdev
+*/
+
+#ifndef _YATO_INTERVAL_MAP_H_
+#define _YATO_INTERVAL_MAP_H_
+
+#include <map>
+#include <limits>
+
+namespace yato
+{
+    /**
+     *  Map storing the whole range of keys in the form of intervals
+     */
+    template <typename _Key, typename _Value, class _Compare = std::less<_Key>, class _Allocator = std::allocator<std::pair<const _Key, _Value>>>
+    class interval_map
+    {
+    public:
+        using key_type = _Key;
+        using mapped_type = _Value;
+        using value_type = std::pair<const _Key, _Value>;
+        using key_compare = _Compare;
+        using allocator_type = _Allocator;
+
+    private:
+        std::map<_Key, _Value> m_map;
+        //-------------------------------------------------------
+
+        template <typename _ValRef>
+        void _init_whole_range(_ValRef && val)
+        {
+            m_map.emplace_hint(m_map.begin(), std::numeric_limits<_Key>::min(), std::forward<_ValRef>(val));
+        }
+        //-------------------------------------------------------
+
+    public:
+        /**
+         * Constructor associates whole range of _Key with val
+         * into the map
+         */
+        explicit
+        interval_map(const mapped_type & val)
+        {
+            _init_whole_range(val);
+        };
+
+        /**
+         * Constructor associates whole range of _Key with val
+         * into the map
+         */
+        explicit
+        interval_map(mapped_type && val)
+        {
+            _init_whole_range(std::move(val));
+        };
+
+        /**
+         * Constructor associates whole range of _Key with val
+         * into the map
+         */
+        interval_map(mapped_type && val, const key_compare & comparator, const allocator_type & allocator = allocator_type())
+            : m_map(comparator, allocator)
+        {
+            _init_whole_range(std::move(val));
+        };
+
+        /**
+         * Assign value val to interval [keyBegin, keyEnd). 
+         * Overwrite previous values in this interval. 
+         * Do not change values outside this interval.
+         * If !( keyBegin < keyEnd ), this designates an empty interval, 
+         * and assign must do nothing.
+         */
+        void assign(const key_type & keyBegin, const key_type & keyEnd, const mapped_type & val)
+        {
+            if (keyBegin < keyEnd) {
+                //Find position for the end point
+                auto endIt = m_map.lower_bound(keyEnd);
+                bool endFound = false;
+                if (endIt != m_map.end()) //check if the next interval has the same value
+                {
+                    //If the next point has the same key and same value 
+                    //then we don't have to insert any new point
+                    if (false == (endIt->first < keyEnd || keyEnd < endIt->first) && endIt->second == val) {
+                        ++endIt;
+                        endFound = true;
+                    }
+                }
+                else //If the last interval has a value equal to 'val',
+                {    //then we don't have to insert any new point
+                    auto tempIt = endIt;
+                    if ((--tempIt)->second == val) {
+                        endFound = true;
+                    }
+                }
+                //If an interval with the same value wasn't found, then check the current position
+                if (false == endFound) {
+                    auto tempIt = endIt;
+                    --tempIt;
+                    //Insert new point if 'keyEnd' doesn't exist or 
+                    //the previous interval's value is not equal to 'val'.
+                    //If the end key exists then the intervals [keyBegin, keyEnd) and [keyEnd, <some key>) 
+                    //join tightly and no changes are needed
+                    if (false == ((endIt != m_map.end() && false == (endIt->first < keyEnd || keyEnd < endIt->first)) || (tempIt->second == val))) {
+                        endIt = m_map.emplace_hint(endIt, keyEnd, tempIt->second);
+                    }
+                }
+                //Find position for the begin point
+                auto beginIt = m_map.lower_bound(keyBegin);
+                bool beginFound = false;
+                //Check if the previous interval's value is equal to 'val'
+                if (beginIt != m_map.begin()) {
+                    auto beginPrev = beginIt;
+                    --beginPrev;
+                    if (beginPrev->second == val) {
+                        //The begin point shifts to the previous interval's begin
+                        //We don't have to insert any new point
+                        beginIt = beginPrev;
+                        beginFound = true;
+                    }
+                }
+                //If an interval with the same value wasn't found, then check the current position
+                if (false == beginFound) {
+                    //If there is there is a point with the same key, then only change the value
+                    if (beginIt != m_map.end() && false == (beginIt->first < keyBegin || keyBegin < beginIt->first)) {
+                        beginIt->second = val;
+                    }
+                    else //otherwise insert new point
+                    {
+                        beginIt = m_map.emplace_hint(beginIt, keyBegin, val);
+                    }
+                }
+                //Erase all points covered by the new interval
+                m_map.erase(++beginIt, endIt);
+            }
+        }
+
+        /**
+         * Look-up of the value associated with key
+         */
+        const mapped_type & operator[](const key_type & key) const
+        {
+            return (--m_map.upper_bound(key))->second;
+        }
+    };
+}
+
+#endif
---
+++ tests/source/test_interval_map.cpp
@@ -0,0 +1,162 @@
+#include "gtest/gtest.h"
+
+#include <array>
+#include <cstdlib>
+
+#include <yato/interval_map.h>
+
+TEST(Yato_IntervalMap, common1)
+{
+    yato::interval_map<size_t, char> myMap('a');
+    EXPECT_EQ('a', myMap[1]);
+}
+
+TEST(Yato_IntervalMap, common2)
+{
+    yato::interval_map<int, char> myMap('A');
+    myMap.assign(0, 100, 'B');
+
+    EXPECT_EQ('A', myMap[-1]);
+    EXPECT_EQ('B', myMap[0]);
+    EXPECT_EQ('B', myMap[2]);
+    EXPECT_EQ('A', myMap[100]);
+    EXPECT_EQ('A', myMap[101]);
+}
+
+TEST(Yato_IntervalMap, common3)
+{
+    yato::interval_map<int, char> myMap('A');
+    myMap.assign(0, 100, 'B');
+    myMap.assign(10, 50, 'C');
+    myMap.assign(40, 120, 'D');
+
+    EXPECT_EQ('B', myMap[0]);
+    EXPECT_EQ('B', myMap[9]);
+    EXPECT_EQ('C',  myMap[10]);
+    EXPECT_EQ('C', myMap[11]);
+    EXPECT_EQ('D', myMap[41]);
+    EXPECT_EQ('D', myMap[50]);
+    EXPECT_EQ('D', myMap[100]);
+    EXPECT_EQ('A', myMap[120]);
+    EXPECT_EQ('A', myMap[121]);
+}
+
+TEST(Yato_IntervalMap, common4)
+{
+    yato::interval_map<int, char> myMap('A');
+    myMap.assign(-2, 0, 'B');
+    myMap.assign(23, 23, 'B');
+
+    EXPECT_EQ('A', myMap[0]);
+    EXPECT_EQ('A', myMap[10]);
+    EXPECT_EQ('A', myMap[100]);
+}
+
+TEST(Yato_IntervalMap, common5)
+{
+    yato::interval_map<unsigned short, char> myMap('A');
+    myMap.assign(10, 20, 'A');
+
+    EXPECT_EQ('A', myMap[0]);
+    EXPECT_EQ('A', myMap[10]);
+    EXPECT_EQ('A', myMap[100]);
+}
+
+TEST(Yato_IntervalMap, common6)
+{
+    yato::interval_map<unsigned short, char> myMap('A');
+    myMap.assign( std::numeric_limits<unsigned short>::min(), std::numeric_limits<unsigned short>::max(), 'B');
+
+    EXPECT_EQ('B', myMap[0]);
+    EXPECT_EQ('B', myMap[10]);
+    EXPECT_EQ('B', myMap[100]);
+    EXPECT_EQ('A', myMap[std::numeric_limits<unsigned short>::max()]);
+}
+
+TEST(Yato_IntervalMap, common7)
+{
+    yato::interval_map<unsigned short, char> myMap('A');
+    myMap.assign(10, 20, 'A');
+
+    EXPECT_EQ('A', myMap[0]);
+    EXPECT_EQ('A', myMap[10]);
+    EXPECT_EQ('A', myMap[100]);
+}
+
+TEST(Yato_IntervalMap, common8)
+{
+    yato::interval_map<unsigned short, char> myMap('A');
+    myMap.assign(0, 10, 'C');
+    myMap.assign(20, 40, 'B');
+    myMap.assign(10, 20, 'C');
+
+    EXPECT_EQ('C', myMap[0]);
+    EXPECT_EQ('C', myMap[9]);
+    EXPECT_EQ('C', myMap[10]);
+    EXPECT_EQ('B', myMap[20]);
+    EXPECT_EQ('B', myMap[39]);
+    EXPECT_EQ('A', myMap[100]);
+}
+
+TEST(Yato_IntervalMap, common9)
+{
+    yato::interval_map<unsigned short, char> myMap('A');
+    myMap.assign(10, 40, 'B');
+    myMap.assign(15, 35, 'B');
+
+    EXPECT_EQ('A', myMap[0]);
+    EXPECT_EQ('A', myMap[9]);
+    EXPECT_EQ('B', myMap[10]);
+    EXPECT_EQ('B', myMap[20]);
+    EXPECT_EQ('B', myMap[39]);
+    EXPECT_EQ('A', myMap[100]);
+}
+
+TEST(Yato_IntervalMap, bruteforce)
+{
+    const size_t N = 100000;
+    for (int t = 0; t < 100; ++t) {
+        //random test set
+        std::array<size_t[3], 100> test;
+        for (size_t i = 0; i < test.size(); ++i) {
+            size_t a = rand() % N;
+            size_t b = rand() % N;
+            size_t v = rand() % 10;
+            test[i][0] = std::min(a, b);
+            test[i][1] = std::max(a, b);
+            test[i][2] = v;
+        }
+        //histogram solution
+        size_t hist[N] = {0};
+        for (size_t i = 0; i < test.size(); ++i) {
+            size_t l = test[i][0];
+            size_t r = test[i][1];
+            size_t v = test[i][2];
+            if (false == l < r) {
+                continue;
+            }
+            for (size_t k = l; k < r; ++k) {
+                hist[k] = v;
+            }
+        }
+        //map solution
+        yato::interval_map<size_t, size_t> myMap(0);
+        for (size_t i = 0; i < test.size(); ++i) {
+            size_t l = test[i][0];
+            size_t r = test[i][1];
+            size_t v = test[i][2];
+            myMap.assign(l, r, v);
+        }
+        //compare
+        bool pass = true;
+        for (size_t i = 0; i < N; ++i) {
+            size_t h = hist[i];
+            size_t m = myMap[i];
+            if (h != m) {
+                pass = false;
+                break;
+            }
+        }
+        EXPECT_TRUE(pass);
+    }
+}
