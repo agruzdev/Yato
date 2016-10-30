80e23ff42615f68386d859d2e5438151fb48bb82
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-10-30 20:54:05 +0300

    Fixed clang toolchain for using the latest clang

--- cmake/clang.toolchain.cmake
+++ cmake/clang.toolchain.cmake
@@ -12,7 +12,17 @@ endif()
 set(CMAKE_C_COMPILER   clang   CACHE FILE "C compiler")
 set(CMAKE_CXX_COMPILER clang++ CACHE FILE "C++ compiler")
 
+if(WIN32)
+    set(target "-target x86_64-mingw32")
+else()
+    set(target "")
+endif()
+
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${target}" CACHE STTRING "C flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${target} -std=c++14 " CACHE STTRING "C++ flags")
 set(CMAKE_C_STANDARD_LIBRARIES   "${CMAKE_C_STANDARD_LIBRARIES}  -lgcc "    CACHE STRING "Standard C Libraries")
 set(CMAKE_CXX_STANDARD_LIBRARIES "${MAKE_CXX_STANDARD_LIBRARIES} -lstdc++ " CACHE STRING "Standard C++ Libraries")
+
+unset(target)
 
 set(CLANG ON)
