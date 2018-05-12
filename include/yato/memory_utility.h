cca3460d1fe7695dbf54cd76c84586080b6babb4
Alexey Gruzdev <alex.gruzdev@hotmail.com>
2018-05-12 22:47:48 +0100

    memory_utility: added fast operations for trivial types

--- include/yato/memory_utility.h
+++ include/yato/memory_utility.h
@@ -8,6 +8,8 @@
 #ifndef _YATO_MEMORY_UTILITY_H_
 #define _YATO_MEMORY_UTILITY_H_
 
+#include <cstring>
+
 #include "type_traits.h"
 #include "assert.h"
 
@@ -19,6 +21,225 @@ namespace yato
      */
     namespace memory
     {
+        /**
+         * copy for trivial types
+         */
+        template <typename Ty_>
+        void trivial_copy(const Ty_* src_first, const Ty_* src_last, Ty_* dst_first)
+        {
+            YATO_REQUIRES(src_first != nullptr);
+            YATO_REQUIRES(src_last  != nullptr);
+            YATO_REQUIRES(dst_first != nullptr);
+            YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
+            YATO_REQUIRES((dst_first - src_last >= 0) || (src_first - (dst_first + std::distance(src_first, src_last)) >= 0));
+
+            std::memcpy(dst_first, src_first, std::distance(src_first, src_last) * sizeof(Ty_));
+        }
+
+        /**
+         * copy_n for trivial types
+         */
+        template <typename Ty_>
+        void trivial_copy_n(const Ty_* src_first, size_t count, Ty_* dst_first)
+        {
+            YATO_REQUIRES(src_first != nullptr);
+            YATO_REQUIRES(dst_first != nullptr);
+            YATO_REQUIRES((dst_first - (src_first + count) >= 0) || (src_first - (dst_first + count) >= 0));
+
+            std::memcpy(dst_first, src_first, count * sizeof(Ty_));
+        }
+
+
+        namespace details
+        {
+            template <typename Alloc_, typename Iter_, typename Ty_, typename = void>
+            struct copy_operation
+            {
+                static
+                void apply(Alloc_ & alloc, Iter_ src_first, Iter_ src_last, Ty_* & dst_first)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    while(src_first != src_last) {
+                        alloc_traits::construct(alloc, dst_first, *src_first++);
+                        ++dst_first;
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Iter_, typename Ty_>
+            struct copy_operation<Alloc_, Iter_, Ty_,
+                    std::enable_if_t<
+                        std::is_trivial<Ty_>::value && 
+                        std::is_pointer<Iter_>::value &&
+                        std::is_same<Ty_, std::remove_cv_t<typename std::iterator_traits<Iter_>::value_type>>::value
+                    >
+                >
+            {
+                static
+                void apply(Alloc_ &, Iter_ src_first, Iter_ src_last, Ty_* & dst_first)
+                {
+                    const size_t count = std::distance(src_first, src_last);
+                    trivial_copy_n(src_first, count, dst_first);
+                    dst_first += count;
+                }
+            };
+
+
+
+            template <typename Alloc_, typename Iter_, typename Ty_, typename = void>
+            struct move_operation
+            {
+                static
+                void apply(Alloc_ & alloc, Iter_ src_first, Iter_ src_last, Ty_* & dst_first)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    while(src_first != src_last) {
+                        alloc_traits::construct(alloc, dst_first, std::move(*src_first++));
+                        ++dst_first;
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Iter_, typename Ty_>
+            struct move_operation<Alloc_, Iter_, Ty_,
+                    std::enable_if_t<
+                        std::is_trivial<Ty_>::value && 
+                        std::is_pointer<Iter_>::value &&
+                        std::is_same<Ty_, std::remove_cv_t<typename std::iterator_traits<Iter_>::value_type>>::value
+                    >
+                >
+            {
+                static
+                void apply(Alloc_ &, Iter_ src_first, Iter_ src_last, Ty_* & dst_first)
+                {
+                    const size_t count = std::distance(src_first, src_last);
+                    trivial_copy_n(src_first, count, dst_first);
+                    dst_first += count;
+                }
+            };
+
+
+
+            template <typename Alloc_, typename Ty_, typename = void>
+            struct destroy_operation
+            {
+                static
+                void apply(Alloc_ & alloc, Ty_* first, Ty_* last)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    while(first != last) {
+                        alloc_traits::destroy(alloc, first++);
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Ty_>
+            struct destroy_operation<Alloc_, Ty_,
+                    std::enable_if_t<std::is_trivial<Ty_>::value>
+                >
+            {
+                static
+                void apply(Alloc_ &, Ty_*, Ty_*)
+                { }
+            };
+
+
+
+            template <typename Alloc_, typename Ty_, typename = void>
+            struct transfer_operation
+            {
+                static
+                void apply(Alloc_ & alloc, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_first)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    while(src_first != src_last) {
+                        alloc_traits::construct(alloc, dst_first, std::move_if_noexcept(*src_first++));
+                        ++dst_first;
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Ty_>
+            struct transfer_operation<Alloc_, Ty_,
+                    std::enable_if_t<std::is_trivial<Ty_>::value>
+                >
+            {
+                static
+                void apply(Alloc_ &, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_first)
+                {
+                    const size_t count = std::distance(src_first, src_last);
+                    trivial_copy_n(src_first, count, dst_first);
+                    dst_first += count;
+                }
+            };
+
+
+
+            template <typename Alloc_, typename Ty_, typename = void>
+            struct transfer_left_operation
+            {
+                static
+                void apply(Alloc_ & alloc, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_first)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    while(src_first != src_last) {
+                        alloc_traits::destroy(alloc, dst_first);
+                        alloc_traits::construct(alloc, dst_first, std::move_if_noexcept(*src_first));
+                        ++dst_first;
+                        ++src_first;
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Ty_>
+            struct transfer_left_operation<Alloc_, Ty_,
+                    std::enable_if_t<std::is_trivial<Ty_>::value>
+                >
+            {
+                static
+                void apply(Alloc_ &, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_first)
+                {
+                    const size_t count = std::distance(src_first, src_last);
+                    std::memmove(dst_first, src_first, count * sizeof(Ty_));
+                    dst_first += count;
+                }
+            };
+
+
+
+            template <typename Alloc_, typename Ty_, typename = void>
+            struct transfer_right_operation
+            {
+                static
+                void apply(Alloc_ & alloc, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_last)
+                {
+                    using alloc_traits = std::allocator_traits<Alloc_>;
+                    const Ty_* src_rfirst = src_first - 1;
+                    const Ty_* src_rlast  = src_last  - 1;
+                    while(src_rfirst != src_rlast) {
+                        alloc_traits::destroy(alloc, dst_last);
+                        alloc_traits::construct(alloc, dst_last, std::move_if_noexcept(*src_rlast));
+                        --dst_last;
+                        --src_rlast;
+                    }
+                }
+            };
+
+            template <typename Alloc_, typename Ty_>
+            struct transfer_right_operation<Alloc_, Ty_,
+                    std::enable_if_t<std::is_trivial<Ty_>::value>
+                >
+            {
+                static
+                void apply(Alloc_ &, const Ty_* src_first, const Ty_* src_last, Ty_* & dst_last)
+                {
+                    const size_t count = std::distance(src_first, src_last);
+                    std::memmove(dst_last - count + 1, src_first, count * sizeof(Ty_));
+                    dst_last -= count;
+                }
+            };
+        }
+
 
         /**
          * Allocate raw memory
@@ -61,6 +282,7 @@ namespace yato
             }
         }
 
+
         /**
          * Init raw memory with copied values.
          * dst_first must not belong [src_first, src_last)
@@ -71,12 +293,9 @@ namespace yato
             YATO_REQUIRES(dst_first != nullptr);
             YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
 
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            while(src_first != src_last) {
-                alloc_traits::construct(alloc, dst_first, *src_first++);
-                ++dst_first;
-            }
+            details::copy_operation<Alloc_, Iter_, Ty_>::apply(alloc, src_first, src_last, dst_first);
         }
+
 
         /**
          * Init raw memory with moved values
@@ -88,15 +307,13 @@ namespace yato
             YATO_REQUIRES(dst_first != nullptr);
             YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
 
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            while(src_first != src_last) {
-                alloc_traits::construct(alloc, dst_first, std::move(*src_first++));
-                ++dst_first;
-            }
+            details::move_operation<Alloc_, Iter_, Ty_>::apply(alloc, src_first, src_last, dst_first);
         }
+
 
         /**
          * Destroy a range of values, leaving memory uninitialized
+         * Does nothing for trivial types
          */
         template <typename Alloc_, typename Ty_>
         void destroy(Alloc_ & alloc, Ty_* first, Ty_* last)
@@ -105,10 +322,7 @@ namespace yato
             YATO_REQUIRES(last  != nullptr);
             YATO_REQUIRES(std::distance(first, last) >= 0);
 
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            while(first != last) {
-                alloc_traits::destroy(alloc, first++);
-            }
+            details::destroy_operation<Alloc_, Ty_>::apply(alloc, first, last);
         }
 
 
@@ -125,11 +339,7 @@ namespace yato
             YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
             YATO_REQUIRES((dst_first - src_last >= 0) || (src_first - dst_first - std::distance(src_first, src_last) >= 0))
             
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            while(src_first != src_last) {
-                alloc_traits::construct(alloc, dst_first, std::move_if_noexcept(*src_first++));
-                ++dst_first;
-            }
+            details::transfer_operation<Alloc_, Ty_>::apply(alloc, src_first, src_last, dst_first);
         }
 
 
@@ -148,15 +358,10 @@ namespace yato
             YATO_REQUIRES(dst_first != nullptr);
             YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
             YATO_REQUIRES(src_first - dst_first > 0);
-            
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            while(src_first != src_last) {
-                alloc_traits::destroy(alloc, dst_first);
-                alloc_traits::construct(alloc, dst_first, std::move_if_noexcept(*src_first));
-                ++dst_first;
-                ++src_first;
-            }
+
+            details::transfer_left_operation<Alloc_, Ty_>::apply(alloc, src_first, src_last, dst_first);
         }
+
 
         /**
          * Transfer a block of values to the right in backward order.
@@ -176,15 +381,7 @@ namespace yato
             YATO_REQUIRES(std::distance(src_first, src_last) >= 0);
             YATO_REQUIRES(dst_last - src_first > 0);
             
-            using alloc_traits = std::allocator_traits<Alloc_>;
-            const Ty_* src_rfirst = src_first - 1;
-            const Ty_* src_rlast  = src_last  - 1;
-            while(src_rfirst != src_rlast) {
-                alloc_traits::destroy(alloc, dst_last);
-                alloc_traits::construct(alloc, dst_last, std::move_if_noexcept(*src_rlast));
-                --dst_last;
-                --src_rlast;
-            }
+            details::transfer_right_operation<Alloc_, Ty_>::apply(alloc, src_first, src_last, dst_last);
         }
 
     }
