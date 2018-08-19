8a3378c96c790925659d50a712d3135e9f459a89
Alexey Gruzdev <alex.gruzdev@hotmail.com>
2018-08-19 17:50:47 +0300

    cmake: dependency_download_and_unzip firstly checks folder, then archive

--- cmake/dependency.common.functions.cmake
+++ cmake/dependency.common.functions.cmake
@@ -18,43 +18,49 @@ function(dependency_folder_not_empty PATH_ RESULT_)
     set(${RESULT_} ${res_} PARENT_SCOPE)
 endfunction()
 
-function(dependency_download_and_unzip URL_ HASH_MD5_ FILE_ UNZIPPED_DIR_)
-    set(download_required_ TRUE)
+# Check that file exists and has valid hash
+function(file_exists STATUS_ FILE_ HASH_MD5_)
+    set(STATUS_ FALSE)
     if(EXISTS ${FILE_})
         file(MD5 ${FILE_} hash_)
         if(${hash_} STREQUAL ${HASH_MD5_})
-            set(download_required_ FALSE)
+            set(STATUS_ TRUE)
         endif()
     endif()
+endfunction()
 
-    if(${download_required_})
-        message(STATUS "Downloading: ${URL_}")
-        file(DOWNLOAD ${URL_} ${FILE_}
-            SHOW_PROGRESS
-            EXPECTED_MD5 ${HASH_MD5_}
-            STATUS ret_
-        )
-        if(NOT ret_)
-            message(FATAL_ERROR "Failed to download: ${URL_}")
-        endif()
-    endif()
 
+function(dependency_download_and_unzip URL_ HASH_MD5_ FILE_ UNZIPPED_DIR_)
     get_filename_component(workdir_ ${FILE_} DIRECTORY)
-    set(output_dir_ ${workdir_}/unzipped)
-    set(${UNZIPPED_DIR_} ${output_dir_} PARENT_SCOPE)
+    set(unzipped_directory ${workdir_}/unzipped)
 
-    dependency_folder_not_empty(${output_dir_} unzip_exists_)
+    dependency_folder_not_empty(${unzipped_directory} unzip_exists_)
     if(NOT ${unzip_exists_})
+        file_exists(archive_is_valid_ ${FILE_} ${HASH_MD5_})
+        if(NOT ${archive_is_valid_})
+            message(STATUS "Downloading: ${URL_}")
+            file(DOWNLOAD ${URL_} ${FILE_}
+                SHOW_PROGRESS
+                EXPECTED_MD5 ${HASH_MD5_}
+                STATUS ret_
+            )
+            if(NOT ret_)
+                message(FATAL_ERROR "Failed to download: ${URL_}")
+            endif()
+        endif()
+
         message(STATUS "Unzipping: ${FILE_}")
-        file(MAKE_DIRECTORY ${output_dir_})
+        file(MAKE_DIRECTORY ${unzipped_directory})
         execute_process(COMMAND "${CMAKE_COMMAND}" -E tar -xzf ${FILE_}
-            WORKING_DIRECTORY ${output_dir_}
+            WORKING_DIRECTORY ${unzipped_directory}
             RESULT_VARIABLE ret_
         )
         if(ret_)
             message(FATAL_ERROR "Failed to extract: ${FILE_}")
         endif()
     endif()
+
+    set(${UNZIPPED_DIR_} ${unzipped_directory} PARENT_SCOPE)
 endfunction()
 
 
@@ -62,13 +68,13 @@ endfunction()
 #function(dependency_find_or_download
 #    DEPENDENCY_NAME           # Short dependency name used for cache variables
 #    DEPENDENCY_VERBOSE_NAME   # Verbose name for messages and hints
-#    DEP_URL           # Url for downloading.
-#    DEP_HASH_MD5      # Hash of downloaded file
-#    FILE_NAME         # Downloaded file name
+#    DEP_URL                   # Url for downloading.
+#    DEP_HASH_MD5              # Hash of downloaded file
+#    FILE_NAME                 # Downloaded file name
 #    
-#    DOWNLOADED_PREFIX     # Additonal path prefix after extracting archive
-#
-#    OUT_INVALIDATED       # Out flag. ON if dependency should be configured. OFF if cache is valid.
+#    DOWNLOADED_PREFIX         # Additonal path prefix after extracting archive
+#   
+#    OUT_INVALIDATED           # Out flag. ON if dependency should be configured. OFF if cache is valid.
 #)
 #
 # Use global flag DOWNLOAD_ALL to enable downloading by default
@@ -87,13 +93,8 @@ function(dependency_find_or_download)
     option(${DEPENDENCY_NAME}_DOWNLOAD "Download ${DEPENDENCY_VERBOSE_NAME} sources" OFF)
     if(${DEPENDENCY_NAME}_DOWNLOAD OR DOWNLOAD_ALL)
         string(TOLOWER ${DEPENDENCY_NAME} folder_name_)
-        if(DEPENDENCY_FILE_NAME)
-            set(file_name_ ${DEPENDENCY_FILE_NAME})
-        else()
-            set(file_name_ "package.zip")
-        endif()
         dependency_download_and_unzip(${DEPENDENCY_URL} ${DEPENDENCY_HASH_MD5} 
-            "${YATO_SOURCE_DIR}/dependencies/${folder_name_}/${file_name_}"
+            "${YATO_SOURCE_DIR}/dependencies/${folder_name_}/package.zip"
             unzipped_location_
         )
         set(root_ ${unzipped_location_})
