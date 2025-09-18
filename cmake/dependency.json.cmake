6dd32f9fe16ddbbfd5eef9919120a20aca031559
Alexey Gruzdev <alex.gruzdev@hotmail.com>
2025-09-18 21:41:20 +0300

    Updated json will v3.12

--- cmake/dependency.json.cmake
+++ cmake/dependency.json.cmake
@@ -18,8 +18,8 @@ include(${YATO_SOURCE_DIR}/cmake/dependency.common.functions.cmake)
 dependency_find_or_download(
     NAME JSON
     VERBOSE_NAME "nlohmann::json"
-    URL "https://github.com/nlohmann/json/releases/download/v3.10.5/include.zip"
-    HASH_MD5 "3cea575f0b559e2609507a5ee6469dea"
+    URL "https://github.com/nlohmann/json/releases/download/v3.12.0/include.zip"
+    HASH_MD5 "b55682fb4e47835e7bbd3da7370c2b83"
 )
 
 set(JSON_INCLUDE_DIR ${JSON_FOUND_ROOT}/include CACHE INTERNAL "")
